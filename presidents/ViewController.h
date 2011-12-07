cdb614e35d50d9fe920a7a253cfb870815632dbe
Will Coughlin <willcoughlin@macbookpro-0662.launchmodem.com>
2011-12-07 17:53:57 -0600

    Initial Commit

---
+++ presidents/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  presidents
+//
+//  Created by Will Coughlin on 12/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
